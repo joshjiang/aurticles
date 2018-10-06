cdc04c7a67cb002be432e123d387019eab35b9e2
Josh Jiang <josh.jiang.dbf@gmail.com>
2018-10-06 15:46:02 -0400

    added articles and user model

--- aurticles/feed/models.py
+++ aurticles/feed/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.utils import timezone
+from django.db import models
+from django.forms import ModelForm
+
+class User(models.Model):
+    time_published = models.DateTimeField('date published')
+    name = models.CharField(max_length = 200, blank = True)
+
+    def was_recently_published(self):
+        return self.time_published  >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return str(self.id)
+
+class Article(models.Model):
+    title = models.CharField(max_length = 500)
+    publisher = models.CharField(max_length = 500)
+
+    text_contents = models.TextField()
+    time_published = models.DateTimeField('date published')
+    time_added = models.DateTimeField('date added')
+    hyperlink = models.URLField()
+
+    # TODO: audio, categories
+    # audio = models.FileField(upload_to=None, max_length=100)
+    # categories = foreignkey models.Categories 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    
+    def __str__(self):
+        return self.name
+
